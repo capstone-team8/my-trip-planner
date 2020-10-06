4e5e56a0178bbf93bf2c4a4cbe2bff494f427d6d
miraenoh <yes_future@naver.com>
2020-10-06 20:40:12 +0900

    메인 페이지 UI

--- client/src/App.vue
+++ client/src/App.vue
@@ -19,6 +19,7 @@ export default {
 /* Import fonts */
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+@import url('https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css');
 
 :root {
 	--my-lightgray: #f1f1f1;
@@ -32,6 +33,30 @@ export default {
 }
 
 body {
-  background: rgba(var(--vs-gray-2));
+  background: rgba(var(--vs-gray-3));
+}
+
+.fullHeight {
+	height: 100%
+}
+
+.vs-table table {
+	min-width: 0 !important;
+}
+
+.card {
+	background-color: white;
+	border-radius: 1em;
+}
+
+::-webkit-scrollbar {
+    width: 5px;
+    height: 5px;
+    display: block;
+}
+
+::-webkit-scrollbar-thumb {
+    background: rgba(var(--vs-gray-4));
+    border-radius: 5px;
 }
 </style>
---
+++ client/src/components/PlanMaker.vue
@@ -0,0 +1,150 @@
+<template>
+	<div class="flex-box">
+		<vs-input icon-after v-model="searchInput" placeholder="장소 검색">
+			<template #icon>
+				<i class="bx bx-search" />
+			</template>
+		</vs-input>
+		<div class="tables">
+			<vs-table v-model="selected">
+				<template #header>
+					검색 결과
+				</template>
+				<template #notFound>
+					방문할 장소를 검색해주세요.
+				</template>
+				<template #tbody>
+					<vs-tr :key="location.id" v-for="location in locations">
+						<vs-td>
+							{{ location.name }}
+						</vs-td>
+						<template #expand>
+							<div class="table-expand">
+								<div>
+									<vs-avatar dark>
+										<template #text>
+											image
+										</template>
+									</vs-avatar>
+								</div>
+								<div class="table-expand-buttons">
+									<vs-tooltip>
+										<vs-button gradient success icon>
+											<i class="bx bxs-map"></i>
+										</vs-button>
+										<template #tooltip>
+											관광지
+										</template>
+									</vs-tooltip>
+									<vs-tooltip>
+										<vs-button gradient primary icon>
+											<i class="bx bxs-moon"></i>
+										</vs-button>
+										<template #tooltip>
+											야경
+										</template>
+									</vs-tooltip>
+									<vs-tooltip>
+										<vs-button gradient danger icon>
+											<i class="bx bx-restaurant"></i>
+										</vs-button>
+										<template #tooltip>
+											식당
+										</template>
+									</vs-tooltip>
+									<vs-tooltip>
+										<vs-button gradient warn icon>
+											<i class="bx bxs-coffee-alt"></i>
+										</vs-button>
+										<template #tooltip>
+											카페
+										</template>
+									</vs-tooltip>
+									<vs-tooltip>
+										<vs-button gradient dark icon>
+											<i class="bx bxs-drink"></i>
+										</vs-button>
+										<template #tooltip>
+											바
+										</template>
+									</vs-tooltip>
+								</div>
+							</div>
+						</template>
+					</vs-tr>
+				</template>
+			</vs-table>
+			<vs-table>
+				<template #header>
+					등록된 여행지
+				</template>
+				<template #notFound>
+					여행할 장소를 등록해주세요.
+				</template>
+				<template #tbody>
+					<vs-tr :key="location.id" v-for="location in locationsSelected">
+						<vs-td>
+							{{ location.name }}
+						</vs-td>
+					</vs-tr>
+				</template>
+			</vs-table>
+		</div>
+		<vs-button class="button-make" flat :active="true">
+			일정 생성
+		</vs-button>
+	</div>
+</template>
+
+<script>
+import dummyLocations from '../../data/dummyLocations'
+
+export default {
+	data: function() {
+		return {
+			searchInput: '',
+			selected: [],
+			locations: dummyLocations,
+			locationsSelected: [dummyLocations[0]]
+		}
+	}
+}
+</script>
+
+<style scoped>
+.vs-table-content {
+	margin-top: 1em;
+}
+
+.flex-box {
+	display: flex;
+	flex-direction: column;
+}
+
+.vs-input-parent {
+	align-self: baseline;
+}
+
+.tables {
+	flex-grow: 1;
+	overflow: auto;
+}
+
+.button-make {
+	width: 7em;
+	align-self: flex-end;
+}
+
+td {
+	text-align: left;
+}
+
+.table-expand {
+	display: flex;
+	justify-content: space-between;
+}
+
+.table-expand-buttons {
+	display: flex;
+}
+</style>
