163229d539ee753afed82c9ded777d4689dab399
kinggodwonho <62094453+kinggodwonho@users.noreply.github.com>
2020-10-21 11:06:34 +0900

    markerfocused 기능 추가
    addMarker 기능 추가
    마커 아이콘 출처: http://maps.google.com/mapfiles/ms/icons/

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -1,12 +1,22 @@
 <template>
 	<div class="container">
+		<button @click="addMarker">마커 등록</button>
 		<GmapMap ref="mapRef" :center="center" :zoom="16" style="width: 100%; height: 100%">
 			<GmapMarker
 				:key="index"
 				v-for="(m, index) in markers"
+				icon="http://maps.google.com/mapfiles/ms/icons/red-dot.png"
 				:position="m.position"
 				:clickabble="true"
-				:draggable="true"
+				:draggable="false"
+			/>
+			<GmapMarker
+				v-if="this.markerFocused!=undefined"
+				icon="http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+				:position= this.markerFocused
+				:clickabble="true"
+				:draggable="false"
+
 			/>
 		</GmapMap>
 	</div>
@@ -18,13 +28,13 @@ export default {
 	props: { markerFocused: Object },
 	data() {
 		return {
-			// Test 좌표
+			// Test Center 좌표
 			center: {
 				lat: 37.293974,
 				lng: 126.975431
 			},
-			// Markers List
-			markers: []
+			// Markers List -> PlanMaker의 location과 통합?
+			markers: [],
 		}
 	},
 	watch: {
@@ -40,13 +50,24 @@ export default {
 	methods: {
 		setPlace(place) {
 			this.$refs.mapRef.$mapPromise.then((map) => {
-				map.panTo(place) // 맵 이동
-				map.setZoom(17)
+				map.panTo(place); // 맵 이동
+				map.setZoom(17);
 			})
-			this.markers.push({
-				position: place
-			})
+		},
+		addMarker(){
+			if (this.markerFocused) {
+       			this.markers.push({
+          			position: this.markerFocused
+					// + 마커 정보
+				});
+				this.markerFocused = undefined;
+      		}
+
 		}
+		// markers 관련 setPlace 메소드만 있는데 선택된 마커 제거하는 메소드 추가
+		//
+		// markers에 여러개 마커가 있을때 해당 마커들을 모두 반영하여 auto zoom, auto center하게(하드코딩x)
+		// -> plan 생성 이후
 	}
 }
 </script>
